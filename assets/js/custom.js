8dfe5dbc857c94ebfdd08981e51341f99ae631b7
Rob Baker <rrbaker@gmail.com>
2016-06-24 14:07:43 -0400

    switched to Bootstrap because Alpaca forms, maker.json edit for example, js edits

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,37 +1,37 @@
 // Example
 $.getJSON("https://raw.githubusercontent.com/rrbaker/maker.json/gh-pages/maker.json", function(json) {
-    $("#example_json").append(JSON.stringify(json, null, "  "));
+	$("#example_json").append(JSON.stringify(json, null, "  "));
 });
 
 // Builder
-$.getJSON("https://raw.githubusercontent.com/rrbaker/maker.json/gh-pages/schemas/maker-v1.json", function(json) {
-    $("#form").alpaca({
-        "schema": json,
-        "options": {
-            "form": {
-                "buttons": {
-                    "submit": {
-                        "title": "Render maker.json",
-                        "styles": "btn btn-primary",
-                        "click": function() {
-                            var val = this.getValue();
-                            if (this.isValid(true)) {
-                                $("#results").text('');
-                                var results = JSON.stringify(val, null, "  ");
-                                $("#results").append(results);
-                                $("#download").removeAttr('disabled');
-                                $("#download_link").attr({
-                                    'href': 'data:text/json;charset=utf-8,' + encodeURIComponent(results),
-                                    'target': '_blank',
-                                    'download': 'maker.json'
-                                });
-                            }
-                        }
-                    }
-                }
+$.getJSON("./schemas/maker-v1.json", function(json) {
+  $("#form").alpaca({
+    "schema": json,
+    "options": {
+      "form": {
+        "btn btn-defaults": {
+          "submit": {
+            "title": "Generate maker.json",
+            "styles": "btn btn-default",
+            "click": function() {
+              var val = this.getValue();
+              if (this.isValid(true)) {
+                $("#results").text('');
+                var results = JSON.stringify(val, null, "  ");
+                $("#results").append(results);
+                $("#download").removeAttr('disabled');
+                $("#download_link").attr({
+                    'href': 'data:text/json;charset=utf-8,' + encodeURIComponent(results),
+                    'target': '_blank',
+                    'download': 'maker.json'
+                });
+              }
             }
-        }
-    });
+          }
+      	}
+    	}
+  	}
+  });
 });
 
 // Validator
@@ -42,16 +42,16 @@ $.getJSON("https://raw.githubusercontent.com/rrbaker/maker.json/gh-pages/schemas
             var valid = tv4.validateMultiple(civic, schema);
             console.log(valid);
             if (valid.valid === true) {
-                $("#validate_result").text("Your maker.json is valid :)");
+                $("#validate_result").addClass("alert alert-success").text("Your maker.json is valid!");
             } else {
-                $("#validate_result").text("Your maker.json is broken :(");
+                $("#validate_result").addClass("alert alert-danger").text("Your maker.json is invalid.");
                 var errors = valid.errors;
                 for (i = 0; i < valid.errors.length; i++) {
                     $("#validate_result").append('<br>' + valid.errors[i].dataPath + ': ' + valid.errors[i].message);
                 }
             }
         } catch (err) {
-            $("#validate_result").addClass("callout alert").text("Sorry, looks like your maker.json is invalid.");
+            $("#validate_result").addClass("alert alert-danger").text("Sorry, looks like your maker.json is invalid.");
         }
     });
 });
